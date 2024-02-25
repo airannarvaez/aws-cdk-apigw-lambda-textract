a8fdc3dae8267cfe331a4c020a0ac19c9b1ad0a5
airan narvaez <airannarvaez@hotmail.com>
2024-02-25 02:49:49 -0300

    inntial commit

---
+++ test/textract.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Textract from '../lib/textract-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/textract-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Textract.TextractStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
